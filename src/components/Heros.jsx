03f9a6b051541e3b95e0bd143da999bb23d27f2b
MiltonBlack <arziblack2@gmail.com>
2024-02-09 14:14:42 +0100

    initial...1

--- src/components/Heros.jsx
+++ src/components/Heros.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
-import { Wallet } from './Wallet'
+// import { Wallet } from './Wallet'
 import { FaCubes, FaTimes } from 'react-icons/fa'
 import modeimg from '../assets/images/heros.jpg'
 
@@ -12,7 +12,7 @@ const Heros = () => {
     return (
         <div className=' relative flex  w-screen h-screen'>
             <div className='fixed right-0 top-0 border text-2xl font-bold font-mono bg-rose-700 m-1 uppercase text-white shadow-2xl shadow-black hover:bg-slate-800 z-10 transition' >
-                <Wallet/> 
+                {/* <Wallet/>  */}
                 </div> 
             <div className=' w-screen h-full'> 
                 <img src={modeimg} alt="img" className=' w-full h-full z-0' />
